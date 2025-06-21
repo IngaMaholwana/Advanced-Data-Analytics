6fdde3ed2a957fdee11af1cbbfb016649635f12e
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-06-21 22:29:40 +0200

    complete data snapshot

--- Automatidata projects/Build_dataframe.py
+++ Automatidata projects/Build_dataframe.py
@@ -2,4 +2,44 @@ import pandas as pd               #library exercise for buidling dataframes
 import numpy as np                #numpy is imported with pandas
 
 df = pd.read_csv('2017_Yellow_Taxi_Trip_Data.csv')
-print(df.head(10))
+# print(df.head(10)) seeing the head
+# print(df.describe())# seeing the tail
+# print(df.info()) #seeing the info
+
+# Sort the data by trip distance from maximum to minimum value
+df_sort = df.sort_values(by=['trip_distance'],ascending=False)
+df_sort.head(10)
+
+# Sort the data by total amount and print the top 20 values
+total_amount_sorted = df.sort_values(
+    ['total_amount'], ascending=False)['total_amount']
+total_amount_sorted.head(20)
+
+# Sort the data by total amount and print the bottom 20 values
+total_amount_sorted.tail(20)
+
+# How many of each payment type are represented in the data?
+df['payment_type'].value_counts()
+
+# What is the average tip for trips paid for with credit card?
+avg_cc_tip = df[df['payment_type']==1]['tip_amount'].mean()
+print('Avg. cc tip:', avg_cc_tip)
+
+# What is the average tip for trips paid for with cash?
+avg_cash_tip = df[df['payment_type']==2]['tip_amount'].mean()
+print('Avg. cash tip:', avg_cash_tip)
+
+# How many times is each vendor ID represented in the data?
+df['VendorID'].value_counts()
+
+# What is the mean total amount for each vendor?
+df.groupby(['VendorID']).mean(numeric_only=True)[['total_amount']]
+
+# Filter the data for credit card payments only
+credit_card = df[df['payment_type']==1]
+
+# Filter the credit-card-only data for passenger count only
+credit_card['passenger_count'].value_counts()
+
+# Calculate the average tip amount for each passenger count (credit card payments only)
+credit_card.groupby(['passenger_count']).mean(numeric_only=True)[['tip_amount']]
