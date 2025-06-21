9894781cea531c2309ea520ca71213c91d4cf295
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-06-21 22:17:32 +0200

    loading the csv day to be used

---
+++ Automatidata projects/Build_dataframe.py
@@ -0,0 +1,5 @@
+import pandas as pd               #library exercise for buidling dataframes
+import numpy as np                #numpy is imported with pandas
+
+df = pd.read_csv('2017_Yellow_Taxi_Trip_Data.csv')
+print("done")
