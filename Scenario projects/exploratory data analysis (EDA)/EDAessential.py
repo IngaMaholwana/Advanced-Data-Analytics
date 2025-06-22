2fd2798f77e41167b9853f255b3fcd269c22efb9
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-06-22 14:22:06 +0200

    completed

--- Scenario projects/exploratory data analysis (EDA)/EDAessential.py
+++ Scenario projects/exploratory data analysis (EDA)/EDAessential.py
@@ -35,3 +35,88 @@ grouped = (companies_sample[["Industry", "years_till_unicorn"]]
           )
 grouped
 # print(grouped)
+
+# Create bar plot
+# with Industry column as the categories of the bars
+# and the difference in years between Year Joined column and Year Founded column as the heights of the bars
+
+
+plt.bar(grouped.index, grouped["years_till_unicorn"])
+
+# Set titling
+
+
+plt.title("Bar plot of maximum years taken by company to become unicorn per industry (from sample)")
+
+# Set x-axis label
+
+
+plt.xlabel("Industry")
+
+# Set y-axis label
+
+
+plt.ylabel("Maximum number of years")
+
+# Rotate labels on the x-axis as a way to avoid overlap in the positions of the text  
+
+
+plt.xticks(rotation=45, horizontalalignment='right')
+#save it
+# plt.savefig("unicorn_years_by_industry.png", dpi=300)
+
+# Display the plot
+
+plt.show()
+
+# Create a column representing company valuation as numeric data
+
+# Create new column
+companies_sample['valuation_billions'] = companies_sample['Valuation']
+# Remove the '$' from each value
+companies_sample['valuation_billions'] = companies_sample['valuation_billions'].str.replace('$', '')
+# Remove the 'B' from each value
+companies_sample['valuation_billions'] = companies_sample['valuation_billions'].str.replace('B', '')
+# Convert column to type int
+companies_sample['valuation_billions'] = companies_sample['valuation_billions'].astype('int')
+companies_sample.head()
+
+# Prepare data for modeling
+grouped = (companies_sample[["Industry", "valuation_billions"]]
+           .groupby("Industry")
+           .max()
+           .sort_values(by="valuation_billions")
+          )
+grouped
+
+# Create bar plot
+# with Industry column as the categories of the bars
+# and new valuation column as the heights of the bars
+
+
+plt.bar(grouped.index, grouped["valuation_billions"])
+
+# Set title
+
+
+plt.title("Bar plot of maximum unicorn company valuation per industry (from sample)")
+
+# Set x-axis label
+
+
+plt.xlabel("Industry")
+
+# Set y-axis label
+
+
+plt.ylabel("Maximum valuation in billions of dollars")
+
+# Rotate labels on the x-axis as a way to avoid overlap in the positions of the text  
+
+
+plt.xticks(rotation=45, horizontalalignment='right')
+
+# Display the plot
+
+
+plt.show()
